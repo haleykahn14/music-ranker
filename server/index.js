f1937e064016d995aa64725083208e0b6eb469de
Haley Kahn <haleykahn14@gmail.com>
2024-07-29 14:42:55 -0400

    changes to endpoint

--- server/index.js
+++ server/index.js
@@ -1,43 +1,98 @@
 import express from 'express';
 import axios from 'axios';
+import dotenv from 'dotenv';
 import qs from 'qs';
 
-const fetch = require
-
 const router = express.Router();
-
-require('dotenv').config();
 
 
 const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
 const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
+var redirect_uri = 'http://localhost:8888/callback';
+var stateKey = 'spotify_auth_state';
 
-async function authenticateToSpotify() {
-  const token_url = 'https://accounts.spotify.com/api/token';
-  const data = qs.stringify({ 'grant_type': 'client_credentials' });
+const generateRandomString = (length) => {
+  return crypto
+  .randomBytes(60)
+  .toString('hex')
+  .slice(0, length);
+}
 
-  const spotifyParams = new URLSearchParams({
-    grant_type: 'authorization_code'
-  })
+router.get('/login', function(req, res) {
 
-  const response = await axios.post(
-    token_url, 
-    spotifyParams,
-    {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));;
+});
+
+router.get('/callback', function(req, res){
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
       headers: {
-        'Authorization': `Basic ${auth_token}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
       },
-      auth: {
-        username: client_id,
-        password: client_secret
-      },
-    },
-  )
-  console.log(response.data.accessToken);
-  return response.data.accessToken;
-};
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
 
 
 router.get('/api/artists', async (req, expressRes) => {
@@ -45,7 +100,7 @@ router.get('/api/artists', async (req, expressRes) => {
   // const artistId = req.params.id; // Use the artist ID from the request parameters
   const artistId = '0TnOYISbd1XYRBk9myaseg';
   const artistUrl = `${process.env.SPOTIFY_ARTIST_URL}${artistId}`;
-  const accessToken = await authenticateToSpotify();
+  // const accessToken = await authenticateToSpotify();
   console.log(accessToken);
 
   try{
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 function App() {
+
+  const authSpotify = async () => {
+    try {
+      const res = await axios.get('/login');
+      console.log(res);
+      const callback = await axios.get('/callback');
+      console.log(callback);
+    }catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   const fetchArtistData = async () => {
     try {
       // Replace '/api/artists' with the correct path and add the artist ID if needed
@@ -9,12 +21,13 @@ function App() {
       console.log(response.data);
       // Handle the response data as needed
     } catch (error) {
-      console.error('Error fetching artist data:', error);
+      console.error('#Error fetching artist data:', error);
     }
   };
 
   return (
     <div>
+      <button onClick={authSpotify}>authorize data</button>
       <button onClick={fetchArtistData}>Fetch Artist Data</button>
     </div>
   );
